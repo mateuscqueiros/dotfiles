ede09ef35c1cb159caedaf2d33e5e26b16cf4b5d
Mateus Queirós <mateuscqueiros@gmail.com>
2024-02-21 22:24:40 -0300

    Priority config

--- nvim/lua/mateus/plugins/colorscheme.lua
+++ nvim/lua/mateus/plugins/colorscheme.lua
@@ -7,17 +7,21 @@ return {
   },
   {
     "folke/tokyonight.nvim",
+    priority = 1000,
     opts = {
       style = "night",
     },
   },
   {
     "craftzdog/solarized-osaka.nvim",
+    priority = 1000,
     opts = {
+      style = "moon",
       transparent = false,
     },
   },
   {
+    "ellisonleao/gruvbox.nvim",
     "altercation/vim-colors-solarized",
     "Alexis12119/nightly.nvim",
     "rose-pine/neovim",
