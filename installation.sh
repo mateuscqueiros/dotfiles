e5c962776d4c4af8d10b93d7d8f2d3801badfb0d
Mateus Queirós <mateuscqueiros@gmail.com>
2024-06-26 01:40:35 -0300

    tpm only plugin

--- installation.sh
+++ installation.sh
@@ -18,9 +18,6 @@ rm -rf ~/.zshrc;
 ln -s ~/.config/zsh/.zshrc ~/.zshrc;
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
 
 zsh;
 
