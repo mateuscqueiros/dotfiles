961d8de13d333ebc468f52b6728ab0dbb05b5daa
Mateus Queirós <mateuscqueiros@gmail.com>
2024-06-26 02:04:58 -0300

    test tmux 7

--- installation.sh
+++ installation.sh
@@ -8,8 +8,9 @@ git clone https://github.com/mateuscqueiros/dotfiles ~/.config
 sudo apt update -y;
 sudo apt install zsh -y;
 
-
 chsh -s $(which zsh)
+
+source ~/.bashrc
 
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y;
 
@@ -29,8 +30,6 @@ sudo apt-get install tmux -y;
 ln -s ~/.config/tmux/.tmux.conf ~/.tmux.conf;
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-exit; exit;
 
 source ~/.zshrc;
 
