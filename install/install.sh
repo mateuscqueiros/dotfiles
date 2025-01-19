f51c9678297dc8260bbcbeb1b02ab71f829d8349
Mateus Queirós <mateuscqueiros@gmail.com>
2025-01-19 13:39:17 -0300

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -32,7 +32,7 @@ fi
 printf "Installed git\n";
 
 sudo add-apt-repository universe;
-sudo apt -qq -y install libfuse2 xclip zip unzip nodejs;
+sudo apt -qq -y install libfuse2 xclip zip unzip;
 
 git clone --quiet https://github.com/mateuscqueiros/dotfiles ~/.config
 
