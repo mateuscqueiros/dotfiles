c84b0c15f02f1896419fe7ea66b656c87f3b4d29
Mateus Queirós <mateuscqueiros@gmail.com>
2025-01-19 13:37:32 -0300

    Create node.sh

---
+++ install/node.sh
@@ -0,0 +1,8 @@
+# Download and install fnm:
+curl -o- https://fnm.vercel.app/install | bash
+# Download and install Node.js:
+fnm install 22
+# Verify the Node.js version:
+node -v # Should print "v22.13.0".
+# Verify npm version:
+npm -v # Should print "10.9.2".
